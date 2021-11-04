d448552027223be15fe6a8c15e64bf6fa306e621
Irene Zaugg <shiliki@gmail.com>
2021-11-04 00:57:50 -0600

    Final edits (comments, cleanup)

--- Exchange/exchange.asm
+++ Exchange/exchange.asm
@@ -12,15 +12,18 @@ number4		word	40
 .code
 main PROC near
 _main:
+	; Move variables into registers for xchg usage.
 	mov		ax, number1
 	mov		bx, number2
 	mov		cx, number3
 	mov		dx, number4
 
+	; Exchange numbers 1 and 4.
 	xchg	ax, dx
 	mov		number1, ax
 	mov		number4, dx
 
+	; Exchange numbers 2 and 3.
 	xchg	bx, cx
 	mov		number2, bx
 	mov		number3, cx
--- Fibonacci/main.asm
+++ Fibonacci/main.asm
@@ -8,27 +8,37 @@ extern _WriteConsoleA@20:near
 prev	dword	1
 curr	dword	1
 next	dword	0
-n		dword	?
-i		byte	3
+
+; Assigning a random value for n for testing purposes.
+; In a normal function call, this would be an unassigned value.
+n		dword	10
+i		dword	3
 
 .code
 main PROC near
 _main:
-	mov		n, 5
 	mov		eax, n
 
+_loop:
+	; Save prev and curr
 	mov		ebx, prev
 	mov		ecx, curr
-	; mov		eax, next
+
+	; Add prev and curr, save to next (next = prev + curr)
+	mov		edx, ebx
+	add		edx, ecx
+	mov		next, edx
+
+	; prev = curr, curr = next
+	mov		prev, ecx
+	mov		curr, edx
+
+	inc		i
+	cmp		eax, i
+	jnz		_loop
+	; End loop
 
 	mov		eax, next
-	add		eax, prev
-	mov		next, eax
-	mov		prev, ecx
-	mov		curr, eax
-
-	jnz		_main
-
 	push	0
 	call	_ExitProcess@4
 
