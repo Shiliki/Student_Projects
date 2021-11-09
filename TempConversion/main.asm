f63a82b99709c2442a7b94aa4f658bd314e21d2b
Irene Zaugg <shiliki@gmail.com>
2021-11-08 19:19:14 -0700

    Add fahrenheit converter

---
+++ TempConversion/main.asm
@@ -0,0 +1,28 @@
+.386P
+.model flat
+extern _ExitProcess@4:near
+
+.data
+	fahren		real8	1.0
+	celsius		real8	?
+	r8_mult		real8	0.555555555555555555556
+	r8_sub		real8	32.0
+
+.code
+main PROC near
+_main:
+; C = (F - 32) x 5 / 9, F = C x 9 / 5 + 32
+	push	ebp
+	mov		ebp, esp
+	
+	fld		fahren
+	fsub	r8_sub
+	fmul	r8_mult
+
+	fst		ebp
+	
+	push	0
+	call	_ExitProcess@4
+
+main ENDP
+END
