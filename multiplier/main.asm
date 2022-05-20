15fd315d2b7ff648b57eb7c2c2959130ed52dce9
Irene Zaugg <shiliki@gmail.com>
2022-05-20 17:27:24 -0600

    Updating Git collection 05-20-22

--- multiplier/main.asm
+++ multiplier/main.asm
@@ -1,37 +1,42 @@
 .386P
 .model flat
 
-include 'writeline.asm'
-
 extern _ExitProcess@4:near
 extern _GetStdHandle@4:near
 extern _WriteConsoleA@20:near
 extern _ExitProcess@4:near
 
-extern _writeline
+extern _writeline:near
 
 .data
-ent		byte	'Enter a number:', 10
-;dp		byte	'Hello World!', 10
-handle	dword	?
-written	dword	?
+prompt		byte	'abc', 10
+msg			byte	'test', 10
+handle		dword	?
+written		dword	?
 
 .code
 main PROC near
 _main:
-	;push	-11
-	;call	_GetStdHandle@4
-	;mov		handle, eax
+	;mov		bh, [prompt]
+	;mov		ecx, 4
+	;call	_writeline
 
-	;push	0
-	;push	offset written
-	;push	15
-	;push	offset ent
-	;push	handle
-	;call	_WriteConsoleA@20
+	mov		msg, bh
+	;mov		numchars, ecx
+	push	-11
+	call	_GetStdHandle@4
+	mov			handle, eax
 
+	; WriteConsole(handle, &msg[0], 13, &written, 0)
+	push	0
+	push	offset written
+	push	13
+	push	offset msg
+	push	handle
+	call	_WriteConsoleA@20
 	push	0
 	call	_ExitProcess@4
 
 main ENDP
+
 END
--- multiplier/writeline.asm
+++ multiplier/writeline.asm
@@ -2,35 +2,37 @@
 .model flat
 
 extern _WriteConsoleA@20:near
-extern _ExitProcess@4:near
 extern _GetStdHandle@4:near
 
 .data
-handle	dword	?
-written	dword	?
-msg		dword	?
+handle		dword	?
+written		dword	?
+msg			byte	'abc'
+numchars	dword	4
 
 .code
 
 ; Precond:
-; ebx = message to be printed
+; bh = message to be printed
 ; ecx = number of bytes in message
 
 _writeline PROC near
-	mov		msg, ebx
+	mov		msg, bh
+	;mov		numchars, ecx
 	push	-11
 	call	_GetStdHandle@4
-	mov		handle, eax
+	mov			handle, eax
 
+	; WriteConsole(handle, &msg[0], 13, &written, 0)
 	push	0
 	push	offset written
-	push	15
+	push	13
 	push	offset msg
 	push	handle
 	call	_WriteConsoleA@20
-
-	push	0
-	call	_ExitProcess@4
+	
+	pop		eax
+	ret
 
 _writeline ENDP
 END
